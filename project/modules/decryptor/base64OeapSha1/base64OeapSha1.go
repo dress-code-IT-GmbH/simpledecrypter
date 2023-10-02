440bc75ccb0e231af77959f411834fa9c83af501
Klaus Kindlinger <klaus.kindlinger@dress-code.biz>
2023-10-02 17:09:31 +0200

    deleted: func B64Decode

--- project/modules/decryptor/base64OeapSha1/base64OeapSha1.go
+++ project/modules/decryptor/base64OeapSha1/base64OeapSha1.go
@@ -7,22 +7,7 @@ import (
 	"dc.local/zlogger"
 	"encoding/base64"
 	"github.com/pkg/errors"
-)
-
-func B64Decode(cipherTextB64Str string) ([]byte, error) {
-	logger := zlogger.GetLogger("decryptor.B64Decode.Decrypt")
-	cipherTextB64 := []byte(cipherTextB64Str)
-	cipherText := make([]byte, base64.StdEncoding.DecodedLen(len(cipherTextB64)))
-	n, e := base64.StdEncoding.Decode(cipherText, cipherTextB64)
-	if e != nil {
-		err := errors.Wrap(e, "base64 decoding failed")
-		logger.Error().Err(err).Msg("")
-		return nil, err
-	}
-	cipherText = cipherText[:n]
-
-	return cipherText, nil
-}
+) // @todo: IMPORT PACKAGE common
 
 func Decrypt(cipherTextB64Str string, key *rsa.PrivateKey) (string, error) {
 	logger := zlogger.GetLogger("decryptor.base64OeapSha1.Decrypt")
--- project/modules/decryptor/base64OeapSha256/base64OeapSha256.go
+++ project/modules/decryptor/base64OeapSha256/base64OeapSha256.go
@@ -9,21 +9,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-func B64Decode(cipherTextB64Str string) ([]byte, error) {
-	logger := zlogger.GetLogger("decryptor.B64Decode.Decrypt")
-	cipherTextB64 := []byte(cipherTextB64Str)
-	cipherText := make([]byte, base64.StdEncoding.DecodedLen(len(cipherTextB64)))
-	n, e := base64.StdEncoding.Decode(cipherText, cipherTextB64)
-	if e != nil {
-		err := errors.Wrap(e, "base64 decoding failed")
-		logger.Error().Err(err).Msg("")
-		return nil, err
-	}
-	cipherText = cipherText[:n]
-
-	return cipherText, nil
-}
-
 func Decrypt(cipherTextB64Str string, key *rsa.PrivateKey) (string, error) {
 	logger := zlogger.GetLogger("decryptor.base64OeapSha256.Decrypt")
 	cipherText, e := B64Decode(cipherTextB64Str) // Hier wird B64Decode aufgerufen
