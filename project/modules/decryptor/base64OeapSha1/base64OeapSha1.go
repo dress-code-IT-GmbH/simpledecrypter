ed3cfaddf1c4610652880d57f32df7ca2e33bc75
war <thomas@warwaris.at>
2023-10-02 17:48:26 +0200

    imports added/cleaned

--- project/modules/decryptor/base64OeapSha1/base64OeapSha1.go
+++ project/modules/decryptor/base64OeapSha1/base64OeapSha1.go
@@ -4,14 +4,16 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha1"
-	"dc.local/zlogger"
 	"encoding/base64"
+
+	"dc.local/decryptor/common"
+	"dc.local/zlogger"
 	"github.com/pkg/errors"
-) // @todo: IMPORT PACKAGE common
+)
 
 func Decrypt(cipherTextB64Str string, key *rsa.PrivateKey) (string, error) {
 	logger := zlogger.GetLogger("decryptor.base64OeapSha1.Decrypt")
-	cipherText, e := B64Decode(cipherTextB64Str) // Hier wird B64Decode aufgerufen
+	cipherText, e := common.B64Decode(cipherTextB64Str) // Hier wird B64Decode aufgerufen
 	clearText, e := rsa.DecryptOAEP(sha1.New(), nil, key, cipherText, []byte(""))
 	if e != nil {
 		err := errors.Wrap(e, "base64 decoding failed")
--- project/modules/decryptor/base64OeapSha1/go.mod
+++ project/modules/decryptor/base64OeapSha1/go.mod
@@ -2,9 +2,11 @@ module dc.local/decryptor/base64OeapSha1
 
 go 1.16
 
+replace dc.local/decryptor/common => ../common
+
 replace dc.local/zlogger => ../../zlogger
 
 require (
 	dc.local/zlogger v0.0.0-00010101000000-000000000000
 	github.com/pkg/errors v0.9.1
-)
+	)
